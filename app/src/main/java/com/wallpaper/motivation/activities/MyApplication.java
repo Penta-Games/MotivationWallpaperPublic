52502220091a9a58c4c1169c9f0f5314d3814230
Aaron <aaron.wtal@googlemail.com>
2019-08-24 17:44:37 +0200

    Neu

---
+++ app/src/main/java/com/wallpaper/motivation/activities/MyApplication.java
@@ -0,0 +1,57 @@
+package com.wallpaper.motivation.activities;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.onesignal.OneSignal;
+
+public class MyApplication extends Application {
+
+    public static final String TAG = MyApplication.class.getSimpleName();
+    private RequestQueue mRequestQueue;
+    private static MyApplication mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+
+        OneSignal.startInit(this)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+
+    }
+
+    public static MyApplication getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/wallpaper/motivation/adapters/AdapterTags.java
@@ -0,0 +1,66 @@
+package com.wallpaper.motivation.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wallpaper.motivation.R;
+import com.wallpaper.motivation.activities.ActivitySearch;
+
+import java.util.ArrayList;
+
+public class AdapterTags extends RecyclerView.Adapter<AdapterTags.ViewHolder> {
+
+    private ArrayList<String> arrayList;
+    Context context;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView txt_tags;
+
+        public ViewHolder(View view) {
+            super(view);
+            txt_tags = view.findViewById(R.id.item_tags);
+        }
+
+    }
+
+    public AdapterTags(Context context, ArrayList<String> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.lsv_item_tag, parent, false);
+        return new AdapterTags.ViewHolder(itemView);
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+
+        ((ViewHolder) holder).txt_tags.setText(arrayList.get(position).toLowerCase());
+
+        ((ViewHolder) holder).txt_tags.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, ActivitySearch.class);
+                intent.putExtra("tags", arrayList.get(position).toLowerCase());
+                context.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+}
